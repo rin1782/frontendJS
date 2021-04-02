168e0e628372e3bafd8d4a4a929fec56bf7d4c98
rin1782 <colaerin@yahoo.com>
2021-04-02 14:43:08 -0400

    started over, created form

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,64 +1,17 @@
-const fosters = [];
-
-function container() {
-    return document.getElementById("container");
-}
-
-function nameInput(){
-    return document.querySelector("#name");
-}
-
-function aboutInput(){
-    return document.querySelector("#about_pet");
-}
-
-function form() {
-    return document.getElementById("form");
-  }
-  
+const endPoint = "http://localhost:3000/pets"
+const container = document.querySelector("div#container")
 
 
 
-function resetInputs(){
-    nameInput().value = "";
-    aboutInput().value = "";
-}
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(endPoint)
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data)
+        // getPets(data)
+    })
+})
 
-function resetContainer() {
-    container().innerHTML = "";
-}
-
-function formTemplate() {
-    return `
-    <form id="form">
-        <h3>Add a new Foster</h3>
-            <div class="inputs">
-                <label for="name">Name</label>
-                <input type="text" name="name" id="name"/>
-            </div>
-            <div class="inputs">
-                <label for="description">Description</label><br>
-                <textarea name="about_pet" id="about_pet" cols="15" rows="5"></textarea>
-            </div>
-            <button type="submit" form="form" value="Submit">Submit</button>
-        </form>
-    `;
-}
-
-function renderForm(){
-    resetContainer();
-    container().innerHTML = formTemplate();
-    form().addEventListener("submit", submitForm);
-}
-
-function submitForm(e){
-    e.preventDefault();
-    fosters.push({
-        name: nameInput().value,
-        description: aboutInput().value,
-    });
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    renderForm();
-});
+// function getPets(p){
+//     p.pets.forEach
+// }
