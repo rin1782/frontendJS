1a76ddeae786b16925e18b86e4396041497a0ea8
rin1782 <colaerin@yahoo.com>
2021-04-07 22:48:34 -0400

    leaving js/rails project off here

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,17 +1,21 @@
 const endPoint = "http://localhost:3000/pets"
-const container = document.querySelector("div#container")
-
-
+const container = document.querySelector("div#petsContainer")
+const newPetsContainer = document.querySelector("div#newPets")
 
 document.addEventListener('DOMContentLoaded', () => {
     fetch(endPoint)
-    .then(resp => resp.json())
+    .then(res => res.json())
     .then(data => {
         console.log(data)
-        // getPets(data)
+        container.innerHTML = data.pets.map(pet => {
+            return `
+        <div data-id="${pet.user.id}">
+            <h2 class="name">${pet.name}</h2>
+            <h4>${pet.about}</h4>
+            <img src=${pet.img_url}/>
+        </div>
+        <button class="delete" data-id="${pet.user.id}">Delete</button>
+        `;
+        })
     })
 })
-
-// function getPets(p){
-//     p.pets.forEach
-// }
--- javascripts/pet.js
+++ javascripts/pet.js
@@ -1,2 +1,73 @@
+class Pet{
+
+    static all = []
+    static container = document.querySelector("div#petsContainer")
+
+    constructor({id, name, about, img_url, user_id}){
+        this.id = id
+        this.name = name
+        this.about = about
+        this.img_url = img_url
+        this.user_id = user_id
+
+        this.element = document.createElement('li')
+        this.element.dataset["id"] = id
+        this.element.id = `${id}`
+
+        
+
+        Pet.all.push(this)
+    }
+}
+
+// creatPet(){
+//     const p = new Pet({id: pet.id, name: pet.name, about: pet.about, img_url: pet.img_url, user_id: pet.user_id})
+//     p.attachToDom()
+// }
+
+function renderPet(){
+    container.innerHTML = `
+        <div data-id="${this.id}">
+            <h2 class="name">${this.name}</h2>
+            <h4>${this.about}<h4>
+            <img src=${this.img_url}/>
+        </div>
+        <button class="delete" data-id="${this.id}">Delete</button>
+        `
+
+        return this.element
+}
+
+// handleClick = (e) => {
+//     if (e.target.innerText === "Delete") {
+//         this.deletePet(e)
+//     }
+// }
+
+// attachToDom() 
+//     this.renderPet()
+//     Pet.container.appendChild(this.element)
+
+//     this.element.addEventListener('click', handleClick)
+// }
+
+// handleClick = (e) => {
+//     if (e.target.innerText === "Delete") {
+//         this.deletePet(e)
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
