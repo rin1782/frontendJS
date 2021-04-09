63f19daa14c5370de9e8ef243a3a0bad726265b1
rin1782 <colaerin@yahoo.com>
2021-04-09 13:10:19 -0400

    successfully worked through JSON

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,6 +2,7 @@ const endPoint = "http://localhost:3000/pets"
 const container = document.querySelector("div#petsContainer")
 const newPetsContainer = document.querySelector("div#newPets")
 
+
 document.addEventListener('DOMContentLoaded', () => {
     fetch(endPoint)
     .then(res => res.json())
@@ -12,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
         <div data-id="${pet.user.id}">
             <h2 class="name">${pet.name}</h2>
             <h4>${pet.about}</h4>
-            <img src=${pet.img_url}/>
+            <img src="${pet.img_url}" height=250px width=300px/>
         </div>
         <button class="delete" data-id="${pet.user.id}">Delete</button>
         `;
