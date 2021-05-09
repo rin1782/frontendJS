6d0990d62baa31cd38417c61bcb378216b7c741f
rin1782 <colaerin@yahoo.com>
2021-05-09 16:19:02 -0400

    updating index.js

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,23 +1,3 @@
-const endPoint = "http://localhost:3000/pets"
-const container = document.querySelector("div#petsContainer")
-const newPetsContainer = document.querySelector("div#newPets")
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    fetch(endPoint)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        container.innerHTML = data.pets.map(pet => {
-            return `
-        <div data-id="${pet.user.id}">
-            <img src="${pet.img_url}" style="border-radius:30%", height=250px width=300px/>
-            <span style="float:right,color:white">
-            <h1>${pet.name}</h1>
-            <p>${pet.about}</p>
-            </span>
-            </div>
-        `;
-        })
-    })
-})
+const usersUrl = "http://localhost:3000/users"
+const petsUrl = "http://localhost:3000/pets"
+const petsForm = document.getElementById("pet-form")
